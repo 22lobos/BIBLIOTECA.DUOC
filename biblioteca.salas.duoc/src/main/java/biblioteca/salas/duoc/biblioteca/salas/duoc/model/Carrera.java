782911325028a9a6086269bbf0485dde339e7cd1
22lobos <cr.loboso@duocuc.cl>
2025-06-22 16:47:24 -0400

    modificacion

--- biblioteca.salas.duoc/src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/model/Carrera.java
+++ biblioteca.salas.duoc/src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/model/Carrera.java
@@ -1,4 +1,9 @@
 package biblioteca.salas.duoc.biblioteca.salas.duoc.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 
 public class Carrera {
     
